c18bcfd1a6b864f196c7cf5161e052bc15f13cf2
finder1793 <kangas.kyle@gmail.com>
2024-12-14 18:44:14 +0000

    1

--- src/main/java/com/github/yufiriamazenta/craftorithm/item/impl/NexoItemProvider.java
+++ src/main/java/com/github/yufiriamazenta/craftorithm/item/impl/NexoItemProvider.java
@@ -36,7 +36,7 @@ public enum NexoItemProvider implements ItemProvider {
             return null;
         }
         ItemStack built = NexoItems.itemFromId(itemName).build();
-        return ItemBuilder.build(built);
+        return finalItemStack!!.clone(built);
     }
 
     @Override
